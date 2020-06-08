9bdd0c3f45ff1245b960e65ae4769687fce2adba
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-06-08 13:05:58 +0300

    Task verification

--- src/by/epamtc/protsko/task01/module4/Task_02.java
+++ src/by/epamtc/protsko/task01/module4/Task_02.java
@@ -1,43 +1,95 @@
 package by.epamtc.protsko.task01.module4;
 
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Year;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 public class Task_02 {
 
-    private static boolean isNonLeapYear() {
-        return ((GregorianCalendar) Calendar.getInstance()).isLeapYear(LocalDateTime.now().getYear());
+    static byte numberOfMonth;
+    static byte dayOfMonth;
+
+    private static String monthName(byte numberOfMonth) {
+        switch (numberOfMonth) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "Mart";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+        }
+        return "Incorrect number of month";
     }
 
-    private static LocalDate getDayMonth(int dayNumber) {
-        int nonLeapYear;
-        if (!isNonLeapYear()) {
-            nonLeapYear = LocalDateTime.now().getYear();
-        } else {
-            nonLeapYear = LocalDateTime.now().getYear() - 1;
+
+    private static int getCountDaysOfMonth(byte numberOfMonth) {
+        switch (numberOfMonth) {
+            case 2:
+                return 28;
+
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+
+            default:
+                System.out.println("Incorrect day of year");
         }
-        Year year = Year.of(nonLeapYear);
-        return year.atDay(dayNumber);
+        return 0;
     }
 
-    static LocalDate getDayMonthByDayNumber(int dayNumber) {
-        if (dayNumber > 0 && dayNumber <= 365) {
-            return getDayMonth(dayNumber);
-        } else {
-            throw new DateTimeException("Invalid value for DayOfYear");
+    private static void getDayOfMonth(int dayOfYear) {
+        numberOfMonth = 1;
+
+        while (dayOfYear > getCountDaysOfMonth(numberOfMonth)) {
+            dayOfYear -= getCountDaysOfMonth(numberOfMonth);
+            numberOfMonth++;
         }
+        dayOfMonth = (byte) dayOfYear;
+    }
+
+    static void getDayMonthByDayNumber(int dayOfYear) {
+        getDayOfMonth(dayOfYear);
+        System.out.println(dayOfMonth + "." + monthName(numberOfMonth));
     }
 
 
     //----- check result -----
     public static void main(String[] args) {
-        LocalDate date = getDayMonthByDayNumber(95);
-        LocalDate date1 = getDayMonthByDayNumber(258);
-        System.out.println(date.getDayOfMonth() + " " + date.getMonth());
-        System.out.println(date1.getDayOfMonth() + " " + date1.getMonth());
+        getDayMonthByDayNumber(15);
+        getDayMonthByDayNumber(90);
+        getDayMonthByDayNumber(128);
+        getDayMonthByDayNumber(243);
+        getDayMonthByDayNumber(246);
+        getDayMonthByDayNumber(273);
+        getDayMonthByDayNumber(274);
+        getDayMonthByDayNumber(303);
+        getDayMonthByDayNumber(304);
+        getDayMonthByDayNumber(305);
+        getDayMonthByDayNumber(365);
     }
 }
